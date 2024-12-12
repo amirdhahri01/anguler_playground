e817f0c323e45357eb085d34ee7110aa57785b4f
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-12 20:32:28 +0100

    An update

--- datatypes.js
+++ datatypes.js
@@ -1,37 +1,56 @@
 "use strict";
-let lname = "Amir";
-let fname = "Dhahri";
-var Fruits;
-(function (Fruits) {
-    Fruits["Banana"] = "Banana";
-    Fruits["Apple"] = "Apple";
-    Fruits["Orange"] = "Orange";
-})(Fruits || (Fruits = {}));
-class Car {
-    constructor(name) {
+// let lname : string = "Amir"
+// let fname : string = "Dhahri"
+// enum Fruits {
+//     Banana = "Banana",
+//     Apple = "Apple",
+//     Orange = "Orange"
+// }
+// interface Icar{
+//     printName():void;
+// }
+// class Car implements Icar{
+//     name : Fruits
+//     constructor(name : Fruits){
+//         this.name = name;
+//     }
+//     printName(){
+//         console.log(this.name);
+//     }
+// }
+// let car : Car = new Car(Fruits.Banana);
+// car.printName();
+// type EmployeeType = {
+//     name : string,
+//     age : number,
+//     country : string
+// }
+// let employees : EmployeeType[] = new Array();
+// employees.push({
+//     name : "Amir Dhahri",
+//     age:20,
+//     country:"Tunisie"
+// })
+// employees.forEach(employee => {
+//     console.log(employee);
+// })
+// function getItem<T> (items : T[] , i : number) : T{
+//     if(i < 0 || i >= items.length)return {} as any;
+//     return items[i];
+// }
+// let items : string[] = ["Amir" , "Dhahri"];
+// let nums : number[] = [1 , 2 , 3 ,  4 ,5 ,6 ,7 ,8,9];
+// console.log(getItem(items , 1));
+// console.log(getItem(nums, 2));
+class Employee {
+    constructor(id, name, address) {
+        this.id = id;
         this.name = name;
-    }
-    printName() {
-        console.log(this.name);
+        this.address = address;
     }
 }
-let car = new Car(Fruits.Banana);
-car.printName();
-let employees = new Array();
-employees.push({
-    name: "Amir Dhahri",
-    age: 20,
-    country: "Tunisie"
-});
-employees.forEach(employee => {
-    console.log(employee);
-});
-function getItem(items, i) {
-    if (i < 0 || i >= items.length)
-        return {};
-    return items[i];
-}
-let items = ["Amir", "Dhahri"];
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(getItem(items, 1));
-console.log(getItem(nums, 2));
+let amir = new Employee(100, "dhah", "qfqfq");
+amir.id = 100;
+amir.name = "AmirDhahri";
+amir.address = "amir@adazd";
+console.log(amir);
