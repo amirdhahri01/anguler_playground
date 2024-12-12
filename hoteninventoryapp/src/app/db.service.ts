0c019667ffc43907c43d3f54f8aca576aa2878f8
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-13 00:48:30 +0100

    An update

---
+++ hoteninventoryapp/src/app/db.service.ts
@@ -0,0 +1,5 @@
+export class DBService{
+    getAll(){
+        return ["C#" , "java" , "PHP" , "Ruby"]; 
+    }
+}
--- hoteninventoryapp/src/app/rooms/rooms.component.ts
+++ hoteninventoryapp/src/app/rooms/rooms.component.ts
@@ -1,16 +1,20 @@
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { DBService } from '../db.service';
 
 @Component({
   selector: 'app-rooms',
-  imports: [],
+  imports: [NgFor],
   standalone: true,
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.scss'
+  styleUrls: ['./rooms.component.scss'],
 })
 
 export class RoomsComponent implements OnInit{
   hotelName = "Hilton Hotel"
+  courses : string[];
   constructor(){
+    this.courses =  new DBService().getAll();
   }
   ngOnInit() : void{
 
