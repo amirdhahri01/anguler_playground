352d71323b7c642ce227530c5a3fd0d02cc9ccbb
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-12 21:05:41 +0100

    An update

--- interface.ts
+++ interface.ts
@@ -1,29 +1,29 @@
-interface User{
-    name : string ,
-    age? : number,
-    id?  : number,
-    email? : string
-}
+// interface User{
+//     name : string ,
+//     age? : number,
+//     id?  : number,
+//     email? : string
+// }
 
-var users : User = {
-    name : "Amir Dhahri" ,
-    age : 20,
-    id : 102,
-    email : "AmirDhahri@gmail.com"    
-}
+// var users : User = {
+//     name : "Amir Dhahri" ,
+//     age : 20,
+//     id : 102,
+//     email : "AmirDhahri@gmail.com"    
+// }
 
-class Employee implements User{
-    name : string;
-    age : number;
-    id: number;
-    email: string;
-    constructor(name : string , age : number , id: number , email: string){
-        this.age = age;
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
-let amir : User = {name : "Amir"};
-console.log(amir);
+// class Employee implements User{
+//     name : string;
+//     age : number;
+//     id: number;
+//     email: string;
+//     constructor(name : string , age : number , id: number , email: string){
+//         this.age = age;
+//         this.name = name;
+//         this.id = id;
+//         this.email = email;
+//     }
+// }
+// let amir : User = {name : "Amir"};
+// console.log(amir);
 
