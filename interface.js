4489c7ab5e880edf8b045eb69b9f1b02c655f404
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-12 20:49:56 +0100

    An update

---
+++ interface.js
@@ -0,0 +1,7 @@
+"use strict";
+var users = {
+    name: "Amir Dhahri",
+    age: 20,
+    id: 102,
+    email: "AmirDhahri@gmail.com"
+};
---
+++ interface.ts
@@ -0,0 +1,13 @@
+interface User{
+    name : string ,
+    age : number,
+    id : number,
+    email : string
+}
+
+var users : User = {
+    name : "Amir Dhahri" ,
+    age : 20,
+    id : 102,
+    email : "AmirDhahri@gmail.com"    
+}
