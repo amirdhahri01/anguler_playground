d82dfaff68fbfe323f3351bfbaed2a470e45aedf
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-12 20:17:04 +0100

    An update

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,19 @@
 "use strict";
 let lname = "Amir";
 let fname = "Dhahri";
+var Fruits;
+(function (Fruits) {
+    Fruits["Banana"] = "Banana";
+    Fruits["Apple"] = "Apple";
+    Fruits["Orange"] = "Orange";
+})(Fruits || (Fruits = {}));
+class Car {
+    constructor(name) {
+        this.name = name;
+    }
+    printName() {
+        console.log(this.name);
+    }
+}
+let car = new Car(Fruits.Banana);
+car.printName();
--- datatypes.ts
+++ datatypes.ts
@@ -1,2 +1,23 @@
 let lname : string = "Amir"
 let fname : string = "Dhahri"
+enum Fruits {
+    Banana = "Banana",
+    Apple = "Apple",
+    Orange = "Orange"
+}
+interface Icar{
+    printName():void;
+}
+class Car implements Icar{
+    name : Fruits
+    constructor(name : Fruits){
+        this.name = name;
+    }
+
+    printName(){
+        console.log(this.name);
+    }
+}
+let car : Car = new Car(Fruits.Banana);
+
+car.printName();
