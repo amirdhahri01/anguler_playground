11b8bee09d4566251d91b203ff32c2c0bc9ec23f
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-13 09:06:38 +0100

    An update

--- hoteninventoryapp/src/app/course/course.component.ts
+++ hoteninventoryapp/src/app/course/course.component.ts
@@ -1,31 +1,39 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'app-course',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss'
 })
 export class CourseComponent {
-    courseName : string = "Anguler 6"
-    imageUrl : string = "https://www.avanse.com/blogs/images/15.jpg"
-    colSpan : number = 3
-    isActive : boolean = true
-    nameval : string = "Amir";
-    methodName(){
-      alert("Welcome to angular")
-    }
-    divClick($event : Event){
-      console.log("Div get fired");
-      console.log($event);
-    }
-    btnClick($event : Event){
-      $event.stopPropagation();
-      console.log("Button get fired");
-      console.log($event);
-    }
+//     courseName : string = "Anguler 6"
+//     imageUrl : string = "https://www.avanse.com/blogs/images/15.jpg"
+//     colSpan : number = 3
+//     isActive : boolean = true
+//     nameval : string = "Amir";
+//     firstname : string = ""
+//     methodName(){
+//       alert("Welcome to angular")
+//     }
+//     divClick($event : Event){
+//       console.log("Div get fired");
+//       console.log($event);
+//     }
+//     btnClick($event : Event){
+//       $event.stopPropagation();
+//       console.log("Button get fired");
+//       console.log($event);
+//     }
     
-    onkeyUp(firstname : string){
-      this.nameval = firstname;
-    }
+//     onkeyUp() : void{
+//       console.log(this.firstname);
+//       this.nameval = this.firstname;
+//     }
+  isFavorite : boolean = false;
+
+  onFavoriteClick() : void{
+    this.isFavorite = !this.isFavorite;
+    console.log(this.isFavorite);
+  }
 }
