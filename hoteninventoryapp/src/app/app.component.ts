1e88e12829d249bceb52a20f322694f6e3c613c3
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-13 12:49:07 +0100

    An update

--- hoteninventoryapp/src/app/app.component.ts
+++ hoteninventoryapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RoomsComponent } from './rooms/rooms.component';
 import { CourseComponent } from './course/course.component';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RoomsComponent , CourseComponent],
+  imports: [RoomsComponent , CourseComponent , ProductsComponent],
   templateUrl: './app.component.html',  
   styleUrl: './app.component.scss',
 })
