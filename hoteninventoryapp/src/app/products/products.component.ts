f6c3726572b2ed3a84cd796c7476dfe5c6c9fe43
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-13 13:45:00 +0100

    An update

--- hoteninventoryapp/src/app/products/products.component.ts
+++ hoteninventoryapp/src/app/products/products.component.ts
@@ -1,13 +1,19 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector : "app-products" ,
-    imports : [],
+    imports : [FormsModule],
     templateUrl: "./products.component.html",
     styleUrls : ["products.component.scss"]
 })
 
 export class ProductsComponent{
+    enteredValue : string = "";
+    onUpdateProductName(event : Event) {
+        this.enteredValue = (<HTMLInputElement>event.target).value ; 
+    }
+    greet : string = "Hello World";
     productId:number = 1;
     stockStatus:string = "Yes";
     addProduct: boolean = true;
@@ -18,5 +24,8 @@ export class ProductsComponent{
     }
     getStock() : string{
         return 'Amir Dhari';
+    }
+    onCLick(){
+        console.log(this.greet);
     }
 }
