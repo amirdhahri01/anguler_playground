0c90365c1b67bb498ec95f83cbd52e85dc0d8e5d
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-13 08:30:49 +0100

    An upadte

--- hoteninventoryapp/src/app/course/course.component.ts
+++ hoteninventoryapp/src/app/course/course.component.ts
@@ -10,4 +10,22 @@ export class CourseComponent {
     courseName : string = "Anguler 6"
     imageUrl : string = "https://www.avanse.com/blogs/images/15.jpg"
     colSpan : number = 3
+    isActive : boolean = true
+    nameval : string = "Amir";
+    methodName(){
+      alert("Welcome to angular")
+    }
+    divClick($event : Event){
+      console.log("Div get fired");
+      console.log($event);
+    }
+    btnClick($event : Event){
+      $event.stopPropagation();
+      console.log("Button get fired");
+      console.log($event);
+    }
+    
+    onkeyUp(firstname : string){
+      this.nameval = firstname;
+    }
 }
