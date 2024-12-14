c8fb1a60ebc7de8e12dc83b0557d7aa0a548e555
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 16:08:09 +0100

    An update

--- hoteninventoryapp/src/app/services/cart.service.ts
+++ hoteninventoryapp/src/app/services/cart.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Product } from '../models/products.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart = signal<Product[]>([])
 
+  addToCart(product : Product){
+    this.cart.set([...this.cart() , product])
+  }
   constructor() { 
 
   }
