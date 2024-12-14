c5f6ddb0691fc78620a64338a5f5a4bfdf586cb1
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 16:55:54 +0100

    An update

--- hoteninventoryapp/src/app/services/cart.service.ts
+++ hoteninventoryapp/src/app/services/cart.service.ts
@@ -6,14 +6,40 @@ import { log } from 'console';
   providedIn: 'root'
 })
 export class CartService {
-  cart = signal<Product[]>([])
+  cart = signal<Product[]>([{
+    id: 1,
+    title: "Sheos",
+    image: "https://brand.assets.adidas.com/image/upload/v1717007627/Training_FW_24_Dropset3_global_Launch_What_shoes_should_you_wear_to_the_gym_image_body1_5efeb22068.jpg",
+    price: 20,
+    stock: 0,
+  },
+  {
+    id: 2,
+    title: "T-shirt",
+    image: "https://brand.assets.adidas.com/image/upload/v1717007627/Training_FW_24_Dropset3_global_Launch_What_shoes_should_you_wear_to_the_gym_image_body1_5efeb22068.jpg",
+    price: 20,
+    stock: 5,
+  },
+  {
+    id: 3,
+    title: "hat",
+    image: "https://brand.assets.adidas.com/image/upload/v1717007627/Training_FW_24_Dropset3_global_Launch_What_shoes_should_you_wear_to_the_gym_image_body1_5efeb22068.jpg",
+    price: 1200,
+    stock: 10,
+  },
+  {
+    id: 4,
+    title: "Sheos",
+    image: "https://brand.assets.adidas.com/image/upload/v1717007627/Training_FW_24_Dropset3_global_Launch_What_shoes_should_you_wear_to_the_gym_image_body1_5efeb22068.jpg",
+    price: 50,
+    stock: 1,
+  }])
 
-  addToCart(product : Product){
-    console.log("Ok.");
-    
-    this.cart.set([...this.cart() , product])
+  addToCart(product: Product) {
+    this.cart.set([...this.cart(), product])
   }
-  constructor() { 
+
+  constructor() {
 
   }
 
