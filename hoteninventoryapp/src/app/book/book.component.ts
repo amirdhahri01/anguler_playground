baaac514f45509fe14a1d7a19ff902590bdd9f74
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 11:49:56 +0100

    An update

--- hoteninventoryapp/src/app/book/book.component.ts
+++ hoteninventoryapp/src/app/book/book.component.ts
@@ -5,24 +5,25 @@ import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-book',
-  imports: [NgClass],
+  imports: [],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
 export class BookComponent {
-  model: BookRepository = new BookRepository();
-  book: Book | undefined = this.model.getIDBook(1);
-  disabled: boolean = true;
-  getClassBinding(id: number): string {
-    this.book = this.model.getIDBook(id);
-    return (this?.book?.price != undefined && this.book.price >= 20) ? "bg-primary" : "bg-info";
-  }
-  getClassObject(id: number): Object {
-    const book = this.model.getIDBook(id);
-    return  {
-      "bg-primary": book?.price != undefined && book?.price >= 20,
-      "bg-info": book?.price != undefined && book?.price < 20,
-      "text-white": book?.name != undefined && book?.name == "My Childhod"
-    };
-  }
+  // model: BookRepository = new BookRepository();
+  // book: Book | undefined = this.model.getIDBook(1);
+  // disabled: boolean = true;
+  // getClassBinding(id: number): string {
+  //   this.book = this.model.getIDBook(id);
+  //   return (this?.book?.price != undefined && this.book.price >= 20) ? "bg-primary" : "bg-info";
+  // }
+  // getClassObject(id: number): Object {
+  //   const book = this.model.getIDBook(id);
+  //   return  {
+  //     "bg-primary": book?.price != undefined && book?.price >= 20,
+  //     "bg-info": book?.price != undefined && book?.price < 20,
+  //     "text-white": book?.name != undefined && book?.name == "My Childhod"
+  //   };
+  // }
+  color:string = "red"
 }
