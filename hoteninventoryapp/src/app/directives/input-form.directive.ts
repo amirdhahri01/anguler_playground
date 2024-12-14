904950776d74b13399c6d0963a672a8e81266da7
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 14:13:28 +0100

    An update

--- hoteninventoryapp/src/app/directives/input-form.directive.ts
+++ hoteninventoryapp/src/app/directives/input-form.directive.ts
@@ -12,6 +12,7 @@ export class InputFormDirective {
   @HostListener("focus") onFocus(){
     console.log("Focus")
   }
+  
   @HostListener("blur") onBlur(){
     let getValue : string = this.ref.nativeElement.value;
     this.ref.nativeElement.value = getValue.toUpperCase();
