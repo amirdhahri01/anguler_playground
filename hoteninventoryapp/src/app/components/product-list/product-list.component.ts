339c47aac1041502473696da7734cfa1b1ad0f4a
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 15:33:12 +0100

    An update

--- hoteninventoryapp/src/app/components/product-list/product-list.component.ts
+++ hoteninventoryapp/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Product } from '../../models/products.model';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-
+  products = signal<Product[]>([
+    {
+      id : 1,
+      title : "Sheos",
+      image : "https://brand.assets.adidas.com/image/upload/v1717007627/Training_FW_24_Dropset3_global_Launch_What_shoes_should_you_wear_to_the_gym_image_body1_5efeb22068.jpg",
+      price : 20,
+      stock  : 2, 
+    }
+  ]);
 }
