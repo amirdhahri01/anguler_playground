c60fe779041710d7ba209b71404324c85e73bbcb
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 14:49:24 +0100

    An update

--- hoteninventoryapp/src/app/app.component.ts
+++ hoteninventoryapp/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CourseComponent } from './course/course.component';
 import { ProductsComponent } from './products/products.component';
 import { BookComponent } from './book/book.component';
 import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RoomsComponent , CourseComponent , ProductsComponent , BookComponent , TestComponent],
+  imports: [RoomsComponent , CourseComponent , ProductsComponent , BookComponent , HeaderComponent],
   templateUrl: './app.component.html',  
   styleUrl: './app.component.scss',
 })
