9f69533d2617f2a5d5728f0e02fb9e755b45143e
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 15:43:52 +0100

    An update

--- hoteninventoryapp/src/app/components/product-card/product-card.component.ts
+++ hoteninventoryapp/src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../models/products.model';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
-
+  product = input.required<Product>()
 }
