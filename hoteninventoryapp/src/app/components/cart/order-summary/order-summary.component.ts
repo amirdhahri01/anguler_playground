c7ed2768a07f0b4539303b5e31cbad1c087b4fb9
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-14 17:10:50 +0100

    An update

--- hoteninventoryapp/src/app/components/cart/order-summary/order-summary.component.ts
+++ hoteninventoryapp/src/app/components/cart/order-summary/order-summary.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
+import { PrimaryButtonComponent } from '../../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss'
 })
 export class OrderSummaryComponent {
-
+  cartService = inject(CartService)
+  total = computed(() => {
+    let total = 0
+    for (const item of this.cartService.cart()) {
+      total += item.price;
+    }
+    return total;
+  })
 }
